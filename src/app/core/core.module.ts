7c080b2c87e8b008be3d6d43e794867f172954ae
Danail Vasilev <massaro@mail.bg>
2018-03-09 12:11:15 +0200

    added toasty service #7

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,12 @@ import { throwIfAlreadyLoaded } from './module-import-guard';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ToastyModule, ToastyService } from 'ng2-toasty';
 import { Api, ImageService } from './services/index';
 
 @NgModule({
   imports: [
+    ToastyModule.forRoot(),
     HttpClientModule,
     RouterModule,
     CommonModule],
@@ -15,7 +17,7 @@ import { Api, ImageService } from './services/index';
   declarations: [
   ],
   exports: [
-    HttpClientModule]
+    HttpClientModule, ToastyModule]
 })
 export class CoreModule {
   constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
--- src/app/image-gallery/image-gallery.component.ts
+++ src/app/image-gallery/image-gallery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../core/services/index';
+import { ToastyService } from 'ng2-toasty';
 import { environment } from '../../environments/environment';
 import { Image, UploadImageResponse } from '../core/interfaces/index';
 
@@ -10,21 +11,33 @@ import { Image, UploadImageResponse } from '../core/interfaces/index';
 })
 export class ImageGalleryComponent implements OnInit {
   title = 'app';
+  isLoading: boolean;
   baseUrl = environment.baseUrl;
   images = [];
 
-  constructor(private imageService: ImageService) {
+  constructor(private imageService: ImageService, private toastyService: ToastyService) {
   }
 
   ngOnInit() {
+    this.isLoading = true;
     this.imageService.getImages().subscribe((_images: Image[]) => {
       console.log('images: ' + _images);
       this.images = _images;
+      this.isLoading = false;
+    }, errorResponse => {
+      this.toastyService.error(`${errorResponse.message}`);
+      this.isLoading = false;
     });
   }
   uploadImage(imageName: string): void {
+    this.isLoading = true;
     this.imageService.uploadImage(imageName).subscribe((result: UploadImageResponse) => {
+      this.toastyService.success(`Image uploaded successfully!`);
       console.log(result);
+      this.isLoading = false;
+    }, errorResponse => {
+      this.toastyService.error(`${errorResponse.message}`);
+      this.isLoading = false;
     });
   }
 }
